2e6b23c4ebdea7d28fa04b5800e0b2d5f02e6a0f
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2015-02-01 09:19:33 +0000

    Add landing page

--- main.rb
+++ main.rb
@@ -4,6 +4,10 @@ def username_for(firstname, lastname, middlename="")
   (firstname[0] + (middlename[0]||"") + lastname[0,5]).downcase
 end
 
+get "/" do
+  "To generate a username, navigate to /your_last_name/your_first_name/optional_middlename to generate a username"
+end
+
 get "/:lastname/:firstname" do
   firstname = params[:firstname]
   lastname = params[:lastname]
